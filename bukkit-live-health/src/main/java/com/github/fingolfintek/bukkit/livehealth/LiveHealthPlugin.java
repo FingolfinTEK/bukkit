9f3da31db6a034c8ef352efed432b25ceb966d00
FingolfinTEK <fingolfintek@gmail.com>
2013-09-24 16:10:57 +0200

    Meh

---
+++ bukkit-live-health/src/main/java/com/github/fingolfintek/bukkit/livehealth/LiveHealthPlugin.java
@@ -0,0 +1,11 @@
+package com.github.fingolfintek.bukkit.livehealth;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Milos.Milivojevic
+ * Date: 24/09/13
+ * Time: 16:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class LiveHealthPlugin extends JavaPlugin {
+}
